37998a78bb0905463c4fa7869904817fb5a365c2
Kondratev <it.fiksik@gmail.com>
2022-11-20 21:17:20 +0400

    add project

---
+++ src/main/java/com/example/mosapiservice/MosApiServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.mosapiservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MosApiServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MosApiServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/mosapiservice/MosApiServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mosapiservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MosApiServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
